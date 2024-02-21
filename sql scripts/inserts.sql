e24fa189e0f350facb2cc9746af71290edca4a1a
nikitagears <macdisk@yandex.ru>
2024-02-20 21:04:38 -0800

    new files

---
+++ sql scripts/inserts.sql
@@ -0,0 +1,201 @@
+
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Margot Robbie', 'female', '07.02.1990');
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Tom Hardy', 'male', '09.15.1977');
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Keanu Reeves', 'male', '09.02.1964');
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Scarlett Johansson', 'female', '11.22.1984');
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Tom Holland', 'male', '06.01.1996');
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Robert Downey', 'male', '04.04.1965');
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Ryan Gosling', 'male', '11.12.1980');
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Laurence Fishburne', 'male', '07.30.1961');
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Jared Leto', 'male', '12.26.1971');
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Ana de Armas', 'female', '04.30.1988');
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Christian Bale', 'male', '01.30.1974');
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Cillian Murphy', 'male', '05.25.1976');
+INSERT INTO "public"."actors" ("actor_name", "gender", "birthday") VALUES ('Bill Murray', 'male', '09.21.1950');
+
+
+
+
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Barbie', 1, 1);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Harley Quinn', 6, 1);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('John Wick', 2, 2);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('John Wick', 3, 2);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('John Wick', 4, 2);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('John Wick', 5, 2);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Evan Webber', 22, 2);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Natasha Romanoff', 17, 3);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Natasha Romanoff', 20, 3);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Peter Parker', 17, 4);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Peter Parker', 19, 4);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Peter Parker', 20, 4);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Nathan Drake', 21, 4);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Tony Stark', 17, 5);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Tony Stark', 18, 5);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Tony Stark', 19, 5);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Tony Stark', 20, 5);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('''K''',, 8, 6);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Ken', 1, 6);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Bowery King', 2, 7);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Bowery King', 3, 7);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Bowery King', 4, 7);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Bowery King', 5, 7);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Joker', 6, 8);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Dr. Michael Morbius', 7, 8);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Niander Wallace', 8, 8);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Nemo', 9, 8);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Joi', 8, 9);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Bel', 22, 9);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Bruce Wayne', 10, 10);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Bruce Wayne', 11, 10);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Bruce Wayne', 12, 10);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Eames', 16, 11);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Bane', 12, 11);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Max Rockatansky', 15, 11);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Eddie Brock', 13, 11);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Eddie Brock', 14, 11);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Robert Fischer', 16, 12);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Dr. Jonathan Crane', 12, 12);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Dr. Jonathan Crane', 10, 12);
+INSERT INTO "public"."characters" ("character_name", "id_movie", "id_actor") VALUES ('Phil', 23, 13);
+
+
+
+
+
+
+
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (4, 1);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (7, 1);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (8, 1);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 2);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (3, 2);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (6, 2);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 3);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (3, 3);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (6, 3);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 4);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (3, 4);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (6, 4);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 5);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (3, 5);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (6, 5);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 6);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (4, 6);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (7, 6);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 7);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (2, 7);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (4, 7);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 8);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (10, 8);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (12, 8);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (10, 9);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (7, 9);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (9, 9);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 10);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (3, 10);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (10, 10);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 11);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (3, 11);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (10, 11);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 12);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (10, 12);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (6, 12);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 13);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (4, 13);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (11, 13);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 14);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (11, 14);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (6, 14);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 15);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (11, 15);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (4, 15);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 16);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (4, 16);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 16);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 17);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (11, 17);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 18);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (4, 18);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (11, 18);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 19);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (4, 19);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (11, 19);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 20);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (4, 20);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (11, 20);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (1, 21);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (4, 21);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (3, 22);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (6, 22);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (8, 23);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (10, 23);
+INSERT INTO "public"."genres_movies" ("id_genre", "id_movie") VALUES (7, 23);
+
+
+
+
+
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Barbie', 2023, 'Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. However, when they get a chance to go to the real world, they soon discover the joys and perils of living among humans.', 6.9);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('John Wick', 2014, 'An ex-hitman comes out of retirement to track down the gangsters who killed his dog and stole his car.', 7.4);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('John Wick 2', 2017, 'After returning to the criminal underworld to repay a debt, John Wick discovers that a large bounty has been put on his life.', 7.4);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('John Wick 3', 2019, 'John Wick is on the run after killing a member of the international assassins guild, and with a $14 million price tag on his head, he is the target of hit men and women everywhere.', 7.4);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('John Wick 4', 2023, 'John Wick uncovers a path to defeating The High Table. But before he can earn his freedom, Wick must face off against a new enemy with powerful alliances across the globe and forces that turn old friends into foes.', 7.7);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Suicide Squad', 2016, 'A secret government agency recruits some of the most dangerous incarcerated super-villains to form a defensive task force. Their first mission: save the world from the apocalypse.', 5.9);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Morbius', 2022, 'Biochemist Michael Morbius tries to cure himself of a rare blood disease, but he inadvertently infects himself with a form of vampirism instead.', 5.2);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Blade Runner 2049', 2017, 'Young Blade Runner discovery of a long-buried secret leads him to track down former Blade Runner Rick Deckard, who been missing for thirty years.', 8.0);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Mr. Nobody', 2009, 'A boy stands on a station platform as a train is about to leave. Should he go with his mother or stay with his father? Infinite possibilities arise from this decision. As long as he doesn choose, anything is possible.', 7.7);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Batman Begins', 2005, 'After witnessing his parents death, Bruce learns the art of fighting to confront injustice. When he returns to Gotham as Batman, he must stop a secret society that intends to destroy the city.', 8.2);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('The Dark Knight', 2008, 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.', 9.0);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('The Dark Knight Rises', 2012, 'Eight years after the Joker reign of chaos, Batman is coerced out of exile with the assistance of the mysterious Selina Kyle in order to defend Gotham City from the vicious guerrilla terrorist Bane.', 8.4);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Venom', 2018, 'A failed reporter is bonded to an alien entity, one of many symbiotes who have invaded Earth. But the being takes a liking to Earth and decides to protect it.', 6.6);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Venom: Let There Be Carnage', 2021, 'Eddie Brock attempts to reignite his career by interviewing serial killer Cletus Kasady, who becomes the host of the symbiote Carnage and escapes prison after a failed execution.', 5.9);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Mad Max: Fury Road', 2012, 'In a post-apocalyptic wasteland, a woman rebels against a tyrannical ruler in search for her homeland with the aid of a group of female prisoners, a psychotic worshiper and a drifter named Max.', 8.1);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Inception', 2010, 'EA thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', 8.8);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('The Avengers', 2012, 'Earth mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.', 8.0);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Iron Man', 2008, 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.', 7.9);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Spider-Man: Homecoming', 2017, 'Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.', 7.4);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Avengers: Age of Ultron', 2015, 'When Tony Stark and Bruce Banner try to jump-start a dormant peacekeeping program called Ultron, things go horribly wrong and it up to Earth mightiest heroes to stop the villainous Ultron from enacting his terrible plan.', 7.3);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Uncharted', 2022, 'Street-smart Nathan Drake is recruited by seasoned treasure hunter Victor "Sully" Sullivan to recover a fortune amassed by Ferdinand Magellan, and lost 500 years ago by the House of Moncada.', 6.3);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Knock Knock', 2015, 'A devoted husband and father helps two stranded young women who knock on his door, but his kind gesture turns into a dangerous seduction and a deadly game of cat and mouse.', 4.9);
+
+INSERT INTO "public"."movies" ("title", "release_date", "desciption", "rating") VALUES ('Groundhog Day', 1993, 'A narcissistic, self-centered weatherman finds himself in a time loop on Groundhog Day.', 8.0);
+
+
+
+
+INSERT INTO "public"."genres" ("genre") VALUES ('Action');
+INSERT INTO "public"."genres" ("genre") VALUES ('Horror');
+INSERT INTO "public"."genres" ("genre") VALUES ('Crime');
+INSERT INTO "public"."genres" ("genre") VALUES ('Adventure');
+INSERT INTO "public"."genres" ("genre") VALUES ('Animation');
+INSERT INTO "public"."genres" ("genre") VALUES ('Thriller');
+INSERT INTO "public"."genres" ("genre") VALUES ('Fantasy');
+INSERT INTO "public"."genres" ("genre") VALUES ('Comedy');
+INSERT INTO "public"."genres" ("genre") VALUES ('Drama');
+INSERT INTO "public"."genres" ("genre") VALUES ('Romance');
+INSERT INTO "public"."genres" ("genre") VALUES ('Sci-Fi');
+INSERT INTO "public"."genres" ("genre") VALUES ('Mystery');
+
+
---
+++ sql scripts/queries.sql
@@ -0,0 +1,43 @@
+--1) Фильмы релиз которых состоялся до 2012 года, с подсчетом количества персонажей сыгравших в фильме
+select "movies"."title" as "title",
+"movies"."release_date" as "release_date",
+"movies"."rating" as "rating",
+COUNT(DISTINCT "characters"."character_name") as count_characters_in_movie
+FROM movies
+LEFT JOIN characters ON "movies"."id_movie"="characters"."id_movie"
+WHERE "movies"."release_date" > '2012'
+GROUP BY "movies"."id_movie"
+ORDER BY count_characters_in_movie desc, "movies"."title" ASC;
+
+
+
+--2) фильмы содержащие слово «день» в названии
+select * from movies where "title" ilike '%day%';
+
+
+
+--3) найти все комедии
+select "movies"."title" as "title",
+"movies"."release_date" as "release_date",
+"movies"."rating" as "rating"
+FROM movies
+LEFT JOIN "genres_movies" ON "movies"."id_movie"="genres_movies"."id_movie"
+LEFT JOIN "genres" ON "genres_movies"."id_genre"="genres"."id_genre"
+WHERE "genres"."genre"='Comedy'
+GROUP BY "movies"."id_movie";
+
+
+
+--4) найти все фильмы с Джаредом Лето
+select "actors"."actor_name", --можно убрать
+"movies"."title" as "title",
+"movies"."release_date" as "release_date",
+"movies"."rating" as "rating",
+"characters"."character_name" -- можно убрать
+FROM movies
+LEFT JOIN "characters" ON "movies"."id_movie"="characters"."id_movie"
+LEFT JOIN "actors" ON "characters"."id_actor"="actors"."id_actor"
+WHERE "actors"."actor_name"='Jared Leto'
+ORDER BY "movies"."release_date" desc; --можно убрать
+
+
